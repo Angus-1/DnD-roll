a12f6b7e12b5a4be545f743fc44b2ac50ab6e4b4
Angel <santoyox714@gmail.com>
2022-10-02 03:39:44 -0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter(),
+  },
 };
 
 export default config;
